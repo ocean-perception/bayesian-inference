ba4bcc8f77f1f8fc2343e418a3a0e50adc7a5dc1
cappelletto <cappelletto@gmail.com>
2020-12-01 12:36:54 +0000

    Regex extraction of base filenames as keys for taret and training dataset matching

--- bnn_geotech.py
+++ bnn_geotech.py
@@ -1,4 +1,5 @@
 # Importe general libraries
+import re
 import sys
 import torch
 import torch.nn as nn
@@ -19,19 +20,41 @@ import matplotlib
 from tools.console import Console
 
 
-def load_dataset (filename, summary=False, latent_name_prefix= 'latent_'):
-    Console.info("load_dataset called for: ", filename)
-    df = pd.read_csv(filename, index_col=0) # use 1t column as ID, the 2nd (relative_path) can be used as part of UUID
-    # The number of 'features' are defined by those columns labeled as 'h'xxx, where xx is 0-based index for the h-latent space vector
+def load_dataset (input_filename, target_filename, matching_key='relative_path', latent_name_prefix= 'latent_'):
+    Console.info("load_dataset called for: ", input_filename)
+
+    df = pd.read_csv(input_filename, index_col=0) # use 1t column as ID, the 2nd (relative_path) can be used as part of UUID
+    # 1) Data validation, remove invalid entries (e.g. NaN)
+    df = df.dropna()
+    Console.info("Total valid entries: ", len(df))
+    # df.reset)index(drop = True) # not sure if we prefer to reset index, as column index was externallly defined
+
+    # 2) Let's determine number of latent-space dimensions
+    # The number of 'features' are defined by those columns labeled as 'relative_path'xxx, where xx is 0-based index for the h-latent space vector
     # Example: (8 dimensions: h0, h1, ... , h7)
     # relative_path northing [m] easting [m] ... latitude [deg] longitude [deg] recon_loss h0 h1 h2 h3 h4 h5 h6 h7
-
-    # 1) Let's determine number of latent-space dimensions
     df_latent = df.filter(regex=latent_name_prefix)
     n_latents = len(df_latent.columns)
-    Console.info ("Latent dimensions: "n_latents)
-    # 2) Dtaa alidation, remove invalid entries (e.g. NaN)
+    Console.info ("Latent dimensions: ", n_latents)
 
+    # 3) Key matching
+    # each 'relative_path' entry has the format  slo/20181121_depthmap_1050_0251_no_slo.tif
+    # where the filename is composed by [date_type_tilex_tiley_mod_type]. input and target tables differ only in 'type' field
+    # let's use regex 
+    # df['filename_base'] = df[matching_key].str.extract(r'(\/.*_)')
+    df['filename_base'] = df[matching_key].str.extract('(?:\/)(.*_)')   # I think it is possible to do it in a single regex
+    df['filename_base'] = df['filename_base'].str.rstrip('_')
+    # print (df['filename_base'])
+
+    tdf = pd.read_csv(target_filename) # expected header: relative_path	mean_slope [ ... ] mean_rugosity
+    tdf = tdf.dropna()
+    target_key='mean_slope'
+    tdf['filename_base'] = tdf[matching_key].str.extract('(?:\/)(.*_)')   # I think it is possible to do it in a single regex
+    tdf['filename_base'] = tdf['filename_base'].str.rstrip('_')
+    Console.info("Target entries: ", len(tdf))
+    # print (tdf['filename_base'])
+    
+    # input-output datasets are linked using the key provided by matching_key
     return 1, n_latents
     # return dataset, num_latents
     # read dataset as pandas dataframe
@@ -74,10 +97,11 @@ def evaluate_regression(regressor,
 def main(args=None):
     # // TODO : add arg parser, admit input file (dataset), config file, validation dataset file, mode (train, validate, predict)
     Console.info("Geotech landability/measurability predictor from low-res acoustics. Uses Bayesian Neural Networks as predictive engine")
-    dataset_filename = "data/koyo-20181121-model-21-output.xls"
+    dataset_filename = "data/koyo-20181121-model-21-output.xls"     # dataset containing the predictive input
+    target_filename = "data/target/koyo20181121-stat-r002-slo.csv"  # output variable to be predicted
     Console.info("Loading dataset: " + dataset_filename)
 
-    Xd, yd = load_dataset(dataset_filename)
+    Xd, yd = load_dataset(dataset_filename, target_filename, matching_key='relative_path')    # relative_path is the common key in both tables
 
     sys.exit(0)
 
@@ -88,12 +112,10 @@ def main(args=None):
 
     X = StandardScaler().fit_transform(X)
     y = StandardScaler().fit_transform(np.expand_dims(y, -1))
-
     X_train, X_test, y_train, y_test = train_test_split(X,
                                                         y,
                                                         test_size=.25,
                                                         random_state=42)
-
 
     X_train, y_train = torch.tensor(X_train).float(), torch.tensor(y_train).float()
     X_test, y_test = torch.tensor(X_test).float(), torch.tensor(y_test).float()
@@ -107,7 +129,6 @@ def main(args=None):
 
     ds_test = torch.utils.data.TensorDataset(X_test, y_test)
     dataloader_test = torch.utils.data.DataLoader(ds_test, batch_size=16, shuffle=True)
-
 
     iteration = 0
     for epoch in range(100):
