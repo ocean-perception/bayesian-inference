e5140f9b7ed25586cea7f13ea6badcc8bc56d899
cappelletto <cappelletto@gmail.com>
2021-08-16 17:23:45 +0100

    Update arg parser to include both training and prediction phase parameters

--- bnn_predict.py
+++ bnn_predict.py
@@ -27,42 +27,94 @@ import statistics
 import math
 
 def add_arguments(obj):
+    # input #########################
+    obj.add_argument(
+        "-i", "--input",
+        type=str,
+        # default='input_latents.csv',
+        help="Path to CSV containing the latent representation vector for each input entry (image). The 'UUID' is used to match against the target file entries"
+    )
+    # latent
     obj.add_argument(
         "-l", "--latent",
         type=str,
-        default='image_latents.csv',
-        help="Path to CSV containing the latent representation for each image. The 'relative_path' will be used to match against the targe_values.csv"
+        default='latent_',
+        help="Name of the key used for the columns containing the latent vector. For example, a h=8 vector should be read as 'latent_0,latent_1,...,latent_7'"
     )
+    # target #########################
     obj.add_argument(
-        "-o", "--output",
-        default='inferred.csv',
+        "-t", "--target",
         type=str,
-        help="File containing the expected and inferred value for each input image (validation + training datasets can be configured)"
+        # default='target_file.csv',
+        help="Path to CSV containing the target entries to be used for training/validation. The 'UUID' is used to match against the input file entries"
     )
-    obj.add_argument(
-        "-n", "--network",
-        default='bnn_trained.pth',
-        type=str,
-        help="Output path to write the trained Bayesian Neural Network, PyTorch compatible format."
-    )
-    obj.add_argument(
-        "-x", "--xinput",
-        default='x_',
-        type=str,
-        help="Define the input vector keyword"
-    )
+    # key #########################
     obj.add_argument(
         "-k", "--key",
         default='measurability',
         type=str,
-        help="Define the keyword that defines the field to be predicted. It must match the column name in the target file"
+        help="Keyword that defines the field to be learnt/predicted. It must match the column name in the target file"
     )
+    # output #########################
+    obj.add_argument(
+        "-o", "--output",
+        # default='inferred.csv',
+        type=str,
+        help="File containing the expected and inferred value for each input entry. It preserves the input file columns and appends the corresponding prediction"
+    )
+    # uuid #########################
+    obj.add_argument(
+        "-u", "--uuid",
+        default='UUID',
+        type=str,
+        help="Unique identifier string used as key for input/target example matching. The UUID string must match for both the input (latent) file and the target file column identifier"
+    )
+    # network #########################
+    obj.add_argument(
+        "-n", "--network",
+        default='bnn_trained.pth',
+        type=str,
+        help="Output path to write the trained Bayesian Neural Network in PyTorch compatible format."
+    )
+    # logfile #########################
+    obj.add_argument(
+        "-g", "--logfile",
+        default='training_log.csv',
+        type=str,
+        help="Output path to the logfile with the training / validation error for each epoch. Used to inspect the training performance"
+    )
+
+    # config #########################
+    obj.add_argument(
+        "-c", "--config",
+#        default='configuration.yaml',
+        type=str,
+        help="Path to YAML configuration file (optional)"
+    )
+    # epochs #########################
+    obj.add_argument(
+        "-e", "--epochs",
+        default='100',
+        type=int,
+        help="Define the number of training epochs"
+    )
+    # samples #########################
     obj.add_argument(
         "-s", "--samples",
-        default='20',
+        default='10',
         type=int,
         help="Define the number of samples for sample_elbo based posterior estimation"
     )
+    # xvalitaion #########################
+
+    obj.add_argument(
+        "-x", "--xratio",
+        default='0.8',
+        type=float,
+        help="Define the training (T) ratio as the proportion of the complete dataset used for training. T + V = 1.0"
+    )
+
+##################################################
 
 
 @variational_estimator
@@ -259,7 +311,7 @@ def main(args=None):
         col_key = 'measurability'
     # user defined keyword (affix) employed to detect the columns containing our input values (latent space representation of the bathymetry images)
     if (args.xinput):
-        input_key = args.key
+        input_key = args.xinput
     else:
         input_key = 'latent_'   # default expected from LGA based pipeline 
 
