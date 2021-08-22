Facultative freeze/unfreeze training phases. Fixed dataloader to catch broken input latent CSV (extra rows). Manual fix of blitz-PR1 for log_posterior numerical convergence issue

blitz/modules/weight_sampler.py
@@ -90,4 +90,4 @@ class PriorWeightDistribution(nn.Module):
 
         prior_pdf = (self.pi * prob_n1 + (1 - self.pi) * prob_n2)
 
-        return (torch.log(prior_pdf) - 0.5).sum()
+        return (torch.log(prior_pdf+1e-6) - 0.5).sum()

bnn_train.py
@@ -181,7 +181,7 @@ def main(args=None):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     regressor = BayesianRegressor(n_latents, 1).to(device)  # Single output being predicted
     # regressor.init
-    optimizer = optim.Adam(regressor.parameters(), lr=0.001) # learning rate
+    optimizer = optim.Adam(regressor.parameters(), lr=0.0015) # learning rate
     criterion = torch.nn.MSELoss()
 
     # print("Model's state_dict:")
@@ -208,9 +208,13 @@ def main(args=None):
 
     print ("ELBO KLD factor: ", elbo_kld/X_train.shape[0]);
     regressor.train()   # set to training mode, just in case
+    regressor.freeze_()
 
     try:
         for epoch in range(num_epochs):
+            if (epoch == 150):          # we train in non-bayesian way during a first phase
+                regressor.unfreeze()
+
             train_loss = []
             # kl_loss = []
             fl_loss = []

tools/bnn_model.py
@@ -12,13 +12,13 @@ class BayesianRegressor(nn.Module):
     def __init__(self, input_dim, output_dim):
         super().__init__()
         # simple 2-layer fully connected linear regressor
-        self.blinear1 = BayesianLinear(64, 64, bias=True, prior_sigma_1=0.5, prior_sigma_2=0.5)
-        # self.blinear2 = BayesianLinear(64, 64, bias=True)
-        # self.blinear2 = BayesianLinear(64, output_dim, bias=True)
-        self.linear_input  = nn.Linear(input_dim, 64, bias=True)
-        self.linear1       = nn.Linear(64, 64, bias=True)
-        # self.linear2       = nn.Linear(64, 64, bias=True)
-        self.linear_output = nn.Linear(64, output_dim, bias=True)
+        self.blinear1 = BayesianLinear(128, 256, bias=True, prior_sigma_1=0.5, prior_sigma_2=0.5)
+        # self.blinear2 = BayesianLinear(128, 128, bias=True)
+        # self.blinear2 = BayesianLinear(128, output_dim, bias=True)
+        self.linear_input  = nn.Linear(input_dim, 128, bias=True)
+        self.linear1       = nn.Linear(256, 128, bias=True)
+        # self.linear2       = nn.Linear(128, 128, bias=True)
+        self.linear_output = nn.Linear(128, output_dim, bias=True)
         self.relu          = nn.LeakyReLU()
 
     def forward(self, x):