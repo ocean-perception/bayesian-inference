Add fixed scalar normalization for output target (tested for slope defined within know range of 0-90)

bnn_train.py
@@ -23,7 +23,6 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 # Helper libaries (viz)
 import matplotlib.pyplot as plt
-import matplotlib                   # TODO: drop
 # Toolkit
 from tools.console import Console
 from tools.dataloader import CustomDataloader
@@ -37,7 +36,6 @@ def handler(signum, frame):
     Console.warn ("CTRL + C pressed. Stopping...")
     exit(1)
 
-
 def main(args=None):
     description_str = "Bayesian Neural Network training module"
     formatter = lambda prog: argparse.HelpFormatter(prog, width=120)
@@ -45,16 +43,14 @@ def main(args=None):
     # argparse.HelpFormatter(parser,'width=120')
     par.add_arguments(parser)
 
-    if len(sys.argv) == 1 and args is None: # no argument passed? error, some parameters were expected
-        # Show help if no args provided
+    Console.info("Bayesian Neural Network for hi-res inference from low resolution acoustic priors (LGA-Bathymetry)")
+    if len(sys.argv) == 1 and args is None: # no argument passed? show help as some parameters were expected
         parser.print_help(sys.stderr)
         sys.exit(2)
     args = parser.parse_args(args)  # retrieve parsed arguments
-    Console.info("Bayesian Neural Network for hi-res inference from low res acoustic priors (LGA-Bathymetry)")
 
     # Start verifying mandatory arguments
-    # let's check if input files exist
-    # [mandatory] input file (latents)
+    # [mandatory] Check if input file (latents) exists
     if os.path.isfile(args.input):
         Console.info("Latent input file:\t", args.input)
     else:
@@ -97,13 +93,14 @@ def main(args=None):
     else:
         n_samples = 10      # default
 
-    Console.info("Geotech landability/measurability predictor from low-res acoustics. Uses Bayesian Neural Networks as predictive engine")
-    dataset_filename = args.input # dataset containing the predictive input. e.g. the latent vector
-    target_filename  = args.target  # output variable to be predicted, e.g. mean_slope
-    # dataset_filename = "data/output-201811-merged-h14.xls"     # dataset containing the predictive input
-    # target_filename = "data/target/koyo20181121-stat-r002-slo.csv"  # output variable to be predicted
+    dataset_filename = args.input   # dataset containing the input. e.g. the latent vector
+    target_filename  = args.target  # target dataset containing the key to be predicted, e.g. mean_slope
     Console.info("Loading dataset: " + dataset_filename)
-    X, y, index_df = CustomDataloader.load_dataset(dataset_filename, target_filename, matching_key=UUID, target_key = output_key, latent_name_prefix=latent_key)    # relative_path is the common key in both tables
+    X, y, index_df = CustomDataloader.load_dataset( input_filename  = dataset_filename, 
+                                                    target_filename = target_filename,
+                                                    matching_key    = UUID,
+                                                    target_key      = output_key,
+                                                    latent_name_prefix = latent_key)    # relative_path is the common key in both tables
     n_latents = X.shape[1]      # this is the only way to retrieve the size of input latent vectors
     Console.info("Data loaded...")
 
@@ -141,54 +138,69 @@ def main(args=None):
     if (args.config):
         Console.warn("Configuration file provided:\t", args.config, " will be ignored (usage not implemented yet)")
 
-
-    # Check optional output filenames, if missing generate the default output names based on the training/data parameters
-    pretrained_network = ""
-
+    # For testing purposes, you can use toydataset loader
     # X, y, index_df = CustomDataloader.load_toydataset(dataset_filename, target_key = output_key, input_prefix= latent_key, matching_key='uuid')    # relative_path is the common key in both tables
 
-    # n_sample = X.shape[0]
+    # To maintain equivalent metrics for normal, log-normal data, we need to normalize the data
+    # However the normalization must be reversible at prediction and interpretration time.
+    # We can used fixed normalization IF we know the data range (e.g. 0-90 degrees for slope)
+
+    # Let's use existing MinMaxScaler, but we need to manually set the parameters
+    # (min, max) = (0, 90)
     # X = StandardScaler().fit_transform(X)
     # y = StandardScaler().fit_transform(np.expand_dims(y, -1)) # this is resizing the array so it can match Size (D,1) expected by pytorch
     # norm = MinMaxScaler().fit(y)
     # y_norm = norm.transform(y)      # min max normalization of our output data
-    y_norm = 1*y
+    # y_norm = y
     # norm = MinMaxScaler().fit(X)
     # X_norm = norm.transform(X)      # min max normalization of our input data
     X_norm = X
 
-    # X_norm = X_ext
-    n_latents = X_norm.shape[1]      # this is the only way to retrieve the size of input latent vectors
+    # We impose fixed normalization for the input data, as we know the expected data range.
+    # Warning: we do not use the data to fit the scaler as there is no guarantee that the ata sample covers all the expected range
+    _d      = np.array([       0.001,         90.0])
+    _log_d  = np.array([np.log(0.001), np.log(90.0)])
+    scaler = MinMaxScaler(feature_range=(0, 1))
+
+    print ("y.shape()", y.shape)
+
+    scaler.fit_transform(_d.reshape(-1, 1))
+
+    y = np.expand_dims(y, -1)
+    print ("y.shape()", y.shape)
+
+    y_norm = scaler.transform(y)
+    print ("y_norm.shape()", y_norm.shape)
+
+    n_latents = X_norm.shape[1]      # retrieve the size of input latent vectors
 
     Console.warn ("Xnorm_Shape", X_norm.shape)
-    # print ("E", X_ext.size)
-
-    print ("X [min,max]", np.amin(X),"/", np.amax(X))
-    print ("X_norm [min,max]", np.amin(X_norm),"/", np.amax(X_norm))
-    print ("Y [min,max]", np.amin(y),"/", np.amax(y))
-    print ("Y_norm [min,max]", np.amin(y_norm),"/", np.amax(y_norm))
+    print ("X [min,max]\t",      np.amin(X),"/",      np.amax(X))
+    print ("X_norm [min,max]\t", np.amin(X_norm),"/", np.amax(X_norm))
+    print ("Y [min,max]\t",      np.amin(y),"/",      np.amax(y))
+    print ("Y_norm [min,max]\t", np.amin(y_norm),"/", np.amax(y_norm))
 
     X_train, X_test, y_train, y_test = train_test_split(X_norm,
                                                         y_norm,
-                                                        test_size=.15, # 3:1 ratio
+                                                        test_size=.2, # 8:2 ratio
                                                         shuffle = True) 
-
+    # Convert train and test vectors to tensors
     X_train, y_train = torch.Tensor(X_train).float(), torch.Tensor(y_train).float()
-    X_test, y_test   = torch.Tensor(X_test).float(),  torch.Tensor(y_test).float()
-
+    X_test,  y_test  = torch.Tensor(X_test).float(),  torch.Tensor(y_test).float()
     y_train = torch.unsqueeze(y_train, -1)  # PyTorch will complain if we feed the (N).Tensor rather than a (NX1).Tensor
-    y_test = torch.unsqueeze(y_test, -1)    # we add an additional dummy dimension
-    # sys.exit(1)
+    y_test =  torch.unsqueeze(y_test, -1)   # we add an additional dummy dimension
+
     if torch.cuda.is_available():
         Console.info("Using CUDA")
+        device = torch.device("cuda")
     else:
         Console.warn("Using CPU")
+        device = torch.device("cpu")
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     regressor = BayesianRegressor(n_latents, 1).to(device)  # Single output being predicted
     # regressor.init
     optimizer = optim.Adam(regressor.parameters(), lr=0.0015) # learning rate
-    criterion = torch.nn.MSELoss()
+    criterion = torch.nn.MSELoss()  # mean squared error loss (squared L2 norm). Used to compute the regression fitting error
 
     # print("Model's state_dict:")
     # for param.Tensor in regressor.state_dict():
@@ -202,60 +214,58 @@ def main(args=None):
 
     iteration = 0
     # Training time
-    test_hist = []
+    test_hist   = []
     uncert_hist = []
-    train_hist = []
-    fit_hist = []
-    ufit_hist = []
+    train_hist  = []
+    fit_hist    = []
+    ufit_hist   = []
+    trfit_hist  = []
+    lambda_fit_loss = 1000.0   # regularization parameter for the fit loss (cost function is the sum of the scaled fit loss and the KL divergence loss)
+    elbo_kld    = 10.0
+    print (regressor)       # show network architecture (this can be retrieved later, but we show it for debug purposes)
 
-    elbo_kld = 1.0
-
-    print (regressor)   # show network architecture
-
-    print ("ELBO KLD factor: ", elbo_kld/X_train.shape[0]);
+    print ("ELBO KLD factor: ", elbo_kld);  # then it needs to be normalized by the number of samples on each dataset (train/test)
     regressor.train()   # set to training mode, just in case
-    regressor.freeze_()
+    regressor.freeze_() # while frozen, the network will behave as a normal network (non-bayesian)
 
     try:
         for epoch in range(num_epochs):
-            if (epoch == 50):          # we train in non-bayesian way during a first phase of P-epochs (P:50)
+            if (epoch == 10):          # we train in non-bayesian way during a first phase of P-epochs (P:50) as 'warm-up'
                 regressor.unfreeze_()
 
             train_loss = []
-            # kl_loss = []
+            kl_loss = []
             fl_loss = []
             for i, (datapoints, labels) in enumerate(dataloader_train):
                 optimizer.zero_grad()
-                
+                # labels.shape = (h,1,1) is adding an extra dimension to the tensor, so we need to remove it
+                labels = labels.squeeze(2)
+                # print ("labels.shape", labels.shape)
                 loss, fit_loss, kl_loss = regressor.sample_elbo(inputs=datapoints.to(device),
                                 labels=labels.to(device),
                                 criterion=criterion,    # MSELoss
                                 sample_nbr=n_samples,
-                                criterion_loss_weight = 200,
+                                criterion_loss_weight = lambda_fit_loss,
                                 complexity_cost_weight=elbo_kld/X_train.shape[0])  # normalize the complexity cost by the number of input points
                 loss.backward() # the returned loss is the combination of fit loss (MSELoss) and complexity cost (KL_div against a nominal Normal distribution )
                 optimizer.step()
                 train_loss.append(loss.item())  # keep track of training loss
                 fl_loss.append(fit_loss.item())
+                # kl_loss.append(kl_loss.item())
 
             test_loss = []  # complete loss for test dataset
             fit_loss = []   # regression (fitting) only loss for test dataset
-            trfit_hist = []
             
             for k, (test_datapoints, test_labels) in enumerate(dataloader_test):
                 # calculate the fit loss and the KL-divergence cost for the test points set
+                test_labels = test_labels.squeeze(2)
+                # print ("test_labels.shape", test_   labels.shape)
                 sample_loss, fit_sample_loss, kl_loss = regressor.sample_elbo(inputs=test_datapoints.to(device),
                                     labels=test_labels.to(device),
                                     criterion=criterion,
                                     sample_nbr=n_samples,
-                                    criterion_loss_weight = 200,   # regularization parameter to balance multiobjective cost function (fit loss vs KL div)
+                                    criterion_loss_weight = lambda_fit_loss,   # regularization parameter to balance multiobjective cost function (fit loss vs KL div)
                                     complexity_cost_weight=elbo_kld/X_test.shape[0])
-
-                # fit_sample_loss = regressor.sample_elbo(inputs=test_datapoints.to(device),
-                #                     labels=test_labels.to(device),
-                #                     criterion=criterion,
-                #                     sample_nbr=n_samples,
-                #                     complexity_cost_weight=0)   # we are interested in the reconstruction/prediction loss only (no KL cost)
 
                 test_loss.append(sample_loss.item())
                 fit_loss.append(fit_sample_loss.item())
@@ -265,7 +275,6 @@ def main(args=None):
 
             mean_train_loss = statistics.mean(train_loss)
             mean_trfit_loss = statistics.mean(fl_loss)
-
 
             mean_fit_loss = statistics.mean(fit_loss)
             stdv_fit_loss = statistics.stdev(fit_loss)
@@ -289,13 +298,15 @@ def main(args=None):
     Console.info("Training completed!")
     torch.save(regressor.state_dict(), network_name)
 
+    type(train_hist)
+    type(trfit_hist)
+    type(test_hist)
+
     export_df = pd.DataFrame([train_hist, trfit_hist, test_hist, uncert_hist, fit_hist, ufit_hist]).transpose()
     export_df.columns = ['train_error', 'train_fit_loss', 'test_error', 'test_error_stdev', 'test_loss', 'test_loss_stdev']
 
     # export_df.index.names=['index']
     export_df.to_csv(logfile_name, index = False)
-    # export_df.to_csv("bnn_train_report.csv")
-    # df = pd.read_csv(input_filename, index_col=0) # use 1t column as ID, the 2nd (relative_path) can be used as part of UUID
 
     # Once trained, we start inferring
     expected = []

tools/bnn_model.py
@@ -20,12 +20,14 @@ from blitz.utils import variational_estimator
 class BayesianRegressor(nn.Module):
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        # simple 2-layer fully connected linear regressor
+
         self.linear_input  = nn.Linear(input_dim, 256, bias=True)
 
-        self.blinear1 = BayesianLinear(256, 512, bias=True, prior_sigma_1=0.5, prior_sigma_2=0.5)
+        self.blinear1 = BayesianLinear(256, 256, bias=True, prior_sigma_1=0.5, prior_sigma_2=0.5)
+        self.silu1         = nn.SiLU()
 
-        self.linear2       = nn.Linear(512, 128, bias=True)
+        self.linear2       = nn.Linear(256, 128, bias=True)
+        self.silu2         = nn.SiLU()
 
         self.linear3       = nn.Linear(128, 64, bias=True)
 
@@ -34,17 +36,16 @@ class BayesianRegressor(nn.Module):
 
         # self.relu          = nn.LeakyReLU()
         # self.relu2         = nn.LeakyReLU()
-        # self.silu1         = nn.SiLU()
 
-    # Oceans architecture: 256 x SiLU | 521 x SiLU | 128 x Lin | 64 x Lin | y: output 
+    # Oceans2021 architecture: 256 x SiLU | 521 x SiLU | 128 x Lin | 64 x Lin | y: output 
 
 
     def forward(self, x):
-        x_ =        self.linear_input (x)
-        x_ = F.silu(self.blinear1     (x_))
-        x_ = F.silu(self.linear2      (x_))
-        x_ =        self.linear3      (x_);
-        x_ =        self.linear_output(x_)
+        x_ =            self.linear_input (x)
+        x_ = self.silu1(self.blinear1     (x_))
+        x_ = self.silu2(self.linear2      (x_))
+        x_ =            self.linear3      (x_);
+        x_ =            self.linear_output(x_)
         return x_
         
 def evaluate_regression(regressor,