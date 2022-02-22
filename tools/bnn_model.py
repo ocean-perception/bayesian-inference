46ec4c40baca9913deff0a9ece25ece7aafe78a6
cappelletto <cappelletto@gmail.com>
2022-02-22 17:05:56 +0000

    Pin working LBSLSLL architecture. Bayesian layer cannot be at the output of the regressor

--- tools/bnn_model.py
+++ tools/bnn_model.py
@@ -24,28 +24,21 @@ class BayesianRegressor(nn.Module):
         # DIM1 = 16
         # DIM2 = 4
         # DIM3 = 2
-        # DIM1 = 64
-        # DIM2 = 32
-        # DIM3 = 16
-        DIM1 = 128
-        DIM2 = 96
+        DIM1 = 256
+        DIM2 = 128
         DIM3 = 64
 
         self.linear_input  = nn.Linear(input_dim, DIM1, bias=True)
 
-        # self.blinear1      = BayesianLinear(DIM1, DIM1, bias=True, prior_sigma_1=0.5, prior_sigma_2=0.5)
-        # self.silu1         = nn.SiLU()
-        # self.sigmoid       = nn.Sigmoid()
-        self.nonlinear1    = nn.Softsign()
+        self.blinear1 = BayesianLinear(DIM1, DIM1, bias=True, prior_sigma_1=0.5, prior_sigma_2=0.5)
+        self.silu1         = nn.SiLU()
 
         self.linear2       = nn.Linear(DIM1, DIM2, bias=True)
-        # self.silu2         = nn.SiLU()
+        self.silu2         = nn.SiLU()
 
-        # self.linear3       = nn.Linear(DIM2, DIM3, bias=True)
+        self.linear3       = nn.Linear(DIM2, DIM3, bias=True)
 
         # self.linear2       = nn.Linear(128, 128, bias=True)
-        self.nonlinear2    = nn.Softsign()
-        self.blinear2      = BayesianLinear(DIM2, DIM3, bias=True, prior_sigma_1=1.0, prior_sigma_2=1.0)
         self.linear_output = nn.Linear(DIM3, output_dim, bias=True)
 
         # self.relu          = nn.LeakyReLU()
@@ -53,13 +46,15 @@ class BayesianRegressor(nn.Module):
 
     # Oceans2021 architecture: 256 x SiLU | 521 x SiLU | 128 x Lin | 64 x Lin | y: output 
 
+
     def forward(self, x):
-        x_ = self.nonlinear1(self.linear_input (x))
-        x_ =                 self.linear2      (x_)
-        x_ = self.nonlinear2(self.blinear2(x_))
-        x_ =                 self.linear_output(x_)
+        x_ =            self.linear_input (x)
+        x_ = self.silu1(self.blinear1     (x_))
+        x_ = self.silu2(self.linear2      (x_))
+        x_ =            self.linear3      (x_);
+        x_ =            self.linear_output(x_)
         return x_
-       
+        
 def evaluate_regression(regressor,
                         X,
                         y,
