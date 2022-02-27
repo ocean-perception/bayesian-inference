aeeb18e3d270c5b0235c738113e0997d410e463b
cappelletto <cappelletto@gmail.com>
2022-02-27 19:37:30 +0000

    Maintenance of dataloader method

--- tools/dataloader.py
+++ tools/dataloader.py
@@ -15,18 +15,19 @@ class CustomDataloader:
     def __init__(self, name=None):
         self.name = " '"  + name + "'" if name else ''
 
-
+    """
+    Dataset loader for oplab pipeline compatible CSV files. The 'matching_key' is used to build the input-output paris (e.g. label for each input row)
+    The name of the target label is defined using 'target_key'
+    """
     def load_dataset (input_filename, target_filename, matching_key='relative_path', target_key ='mean_slope', latent_name_prefix= 'latent_'):
-        Console.info("load_dataset called for: ", input_filename)
 
         df = pd.read_csv(input_filename) # remove index_col=0 when using toy dataset (otherwise it's used as df index and won't be available for query)
         # df = pd.read_csv(input_filename, index_col=0) # use 1st column as ID, the 2nd (relative_path) can be used as part of UUID
 
         # 1) Data validation, remove invalid entries (e.g. NaN)
         df = df.dropna()
-        print (df.head())
+        # print (df.head()) # Enable for debug purposes
         Console.info("Total valid entries: ", len(df))
-        # df.reset)index(drop = True) # not sure if we prefer to reset index, as column index was externallly defined
 
         # 2) Let's determine number of latent-space dimensions
         # The number of 'features' are defined by those columns labeled as 'relative_path'xxx, where xx is 0-based index for the h-latent space vector
@@ -42,31 +43,23 @@ class CustomDataloader:
         print ("target_key: ", target_key)
         print ("latent_name_prefix: ", latent_name_prefix)
 
-        df['matching_key'] = df[matching_key]
+        df['matching_key'] = df[matching_key]   # create a new dataframe column with the matching key
 
         tdf = pd.read_csv(target_filename) # expected header: relative_path	mean_slope [ ... ] mean_rugosity
         tdf = tdf.dropna()
-        tdf['matching_key'] = tdf[matching_key]
+        tdf['matching_key'] = tdf[matching_key] # create the dataframe containing the target values
 
-        # print (tdf.head())    
         Console.info("Target entries: ", len(tdf))
 
-        # print ("****************************************************\n")
-        # print (df.head())
-        # print ("****************************************************\n")
-        # print (tdf.head())
-        # print ("****************************************************\n")
+        merged_df = pd.merge(df, tdf, how='right', on='matching_key')   # join on right, so that we can use the target values
+        merged_df = merged_df.dropna()  # drop any stray NaN values
 
-        merged_df = pd.merge(df, tdf, how='right', on='matching_key')
-        merged_df = merged_df.dropna()
-
-        latent_df = merged_df.filter(regex=latent_name_prefix)
+        latent_df = merged_df.filter(regex=latent_name_prefix)  # remove all columns not starting with latent_name_prefix ('latent_')
         Console.info ("Latent size: ", latent_df.shape)
-        # print (merged_df.head())
-        # print ("****************************************************\n")
+
         target_df = merged_df[target_key]
-        np_latent = latent_df.to_numpy(dtype='float')
-        np_target = target_df.to_numpy(dtype='float')
+        np_latent = latent_df.to_numpy(dtype='float')   # Explicit numeric data conversion to avoid silent bugs with implicit string conversion
+        np_target = target_df.to_numpy(dtype='float')   # Apply to both target and latent data
         # input-output datasets are linked using the key provided by matching_key
         return np_latent, np_target, merged_df['matching_key']
 
