256fa15c36b6469bc80c46dac05f9697deebeb65
Miquel Massot <miquel.massot@gmail.com>
2023-04-18 10:51:03 +0100

    Update README, change setup __version__ handling

--- setup.py
+++ setup.py
@@ -7,64 +7,66 @@
 # package source folder: src/
 # dependencies: numpy, scipy, torch, pandas, scikit-learn, blitz
 
-from setuptools import setup, find_packages
-
-import datetime
 import git
+from setuptools import find_packages, setup
+
 repo = git.Repo(search_parent_directories=True)
 sha = repo.head.object.hexsha
 
-# Prepare version to be PEP 440 compliant with the following format:
-# <year>.<month>.<day>.<release>
-# using the current timestamp as release number and the tag as release
-__version__ = datetime.datetime.now().strftime("%Y.%m.%d.") + sha[:7]
+tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+latest_tag = tags[-1]
+
+# get number of commits since latest tag
+commits_since_tag = repo.iter_commits(latest_tag.commit.hexsha + "..HEAD")
+n_commits = sum(1 for c in commits_since_tag)
+
+# Build version string with the tag + dev + number of commits since tag and hash
+__version__ = latest_tag.name
+if n_commits > 0:
+    __version__ += ".dev" + str(n_commits) + "+" + sha[:7]
 if repo.is_dirty():
     __version__ += ".dirty"
 
-# TODO: Complete git hook to retrieve version from git tags
-# get the version number from the version file
-# with open(os.path.join('src', 'version.py')) as f:
-#     exec(f.read())
 
 def run_setup():
     # get the long description from the README file
     # TODO: Merge with short/specific decription provided during setup() call
-    with open('README.md') as f:
+    with open("README.md") as f:
         long_description = f.read()
     if long_description is None:
-        long_description = 'No description available'
+        long_description = "No description available"
 
     setup(
-        name='bnn_inference',
+        name="bnn_inference",
         version=__version__,
-        description='Bayesian NN training/inference engine to learn mappings between latent representations of low resolution maps and high resolution maps',
-        author='Jose Cappelletto',
-        author_email='j.cappelletto@soton.ac.uk',
-        url='https://github.com/cappelletto/bayesian_inference',
-        license='GPLv3', # check if oplab requires MIT for all packages
-        packages=find_packages(where='src'),
+        description="Bayesian NN training/inference engine to learn mappings between latent representations of low resolution maps and high resolution maps",
+        author="Jose Cappelletto",
+        author_email="j.cappelletto@soton.ac.uk",
+        url="https://github.com/cappelletto/bayesian_inference",
+        license="GPLv3",  # check if oplab requires MIT for all packages
+        packages=find_packages(where="src"),
         package_dir={"": "src"},
         entry_points={
-            'console_scripts': [
-                'bnn_inference = bnn_inference.cli:main',
+            "console_scripts": [
+                "bnn_inference = bnn_inference.cli:main",
             ],
         },
-
         # TODO: need to recreate dep matrix.
         # Pytorch does not support CUDA 11.5, using older 10.2 for the conda env
-        install_requires = [
+        install_requires=[
             "blitz-bayesian-pytorch==0.2.7",
-            "numpy>=1.19.0", # check for update of pandas
+            "numpy>=1.19.0",  # check for update of pandas
             "pandas>=0.25.3",
             "torch>=1.7.0",  # check for a newer version of torch, supporting multi-GPU and improved queries on allocated memory
             "torchvision>=0.8.1",
-            "scikit-learn>=0.23.1", # scikit was bumped to 1.XX series. Maybe worth doing the update with the rest of the packages
+            "scikit-learn>=0.23.1",  # scikit was bumped to 1.XX series. Maybe worth doing the update with the rest of the packages
             "Pillow>=9.1.1",
             "scipy>=1.5.0",
             "typer>=0.7.0",
             "gitpython>=3.1.14",
-        ]
-)
+        ],
+    )
+
 
 if __name__ == "__main__":
     run_setup()
